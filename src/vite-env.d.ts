8396836d320932b40148a5476d5721d480d3a035
cheng-alain <Alain.cheng@protonmail.com>
2026-04-15 23:35:00 +0200

    fix: update Hero component to support video playback and enhance layout

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -39,3 +39,13 @@ declare module "*.ico" {
   const src: string;
   export default src;
 }
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webm" {
+  const src: string;
+  export default src;
+}
